fa48a19f2bfe4ed7e1b98aea01779bc7c22364c4
Carlos Eduardo <ducameneses16@gmail.com>
2024-02-13 20:27:42 -0300

    feat: Implementing AppAuctionDbContext

---
+++ src/Auction.API/Repositories/AppAuctionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AppAuction.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppAuction.API.Repositories;
+
+public class AppAuctionDbContext : DbContext
+{
+    public AppAuctionDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Auction> Auctions { get; set; }
+    public DbSet<User> Users { get; set; }  
+    public DbSet<Offer> Offers { get; set; }  
+
+}
+    

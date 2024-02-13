a8677775c238ae33f5bcc897a01fc71e458620cb
Carlos Eduardo <ducameneses16@gmail.com>
2024-02-13 20:23:42 -0300

    feat: Implementing the UsesCases

---
+++ src/Auction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,15 @@
+﻿using AppAuction.API.Contracts;
+using AppAuction.API.Entities;
+using AppAuction.API.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase
+{
+    private readonly IAuctionRepository _repository;
+
+    public GetCurrentAuctionUseCase(IAuctionRepository repository) => _repository = repository;
+
+    public Auction? Execute() => _repository.GetCurrent();
+}
---
+++ src/Auction.API/UseCases/Offers/CreateOffer/CreateOfferUseCase.cs
@@ -0,0 +1,36 @@
+﻿using AppAuction.API.Comunication.Request;
+using AppAuction.API.Contracts;
+using AppAuction.API.Entities;
+using AppAuction.API.Repositories;
+using AppAuction.API.Services;
+
+namespace AppAuction.API.UseCases.Offers.CreateOffer;
+
+public class CreateOfferUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+    private readonly IOfferRepository _repository;
+
+    public CreateOfferUseCase(ILoggedUser loggedUser, IOfferRepository repository)
+    {
+        _loggedUser = loggedUser;
+        _repository = repository;
+    }
+
+    public Offer Execute(int itemId, RequestCreateOfferJson request)
+    {
+        var user = _loggedUser.GetUserAuthorized();
+
+        var offer = new Offer
+        {
+            CreatedOn = DateTime.Now,
+            ItemId = itemId,
+            Price = request.Price,
+            UserId = user.Id
+        };
+        _repository.Add(offer);
+
+        return offer;
+    }
+
+}
